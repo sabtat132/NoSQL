555e7b290b6c7999492ba73656d2ddcddbab14f0
sabtat132 <46466148+sabtat132@users.noreply.github.com>
2020-09-16 12:30:07 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,30 +3,21 @@
 var express = require('express');
 const fs = require('fs');
 const validator = require('validator');
+const notes = require('./notes.js');
 
-var modules = require('./lib/modules.js');
+const message = notes();
+console.log(message);
 
 var app = express();
 
-app.use(express.static(__dirname + "/style"));
-app.use(express.static(__dirname + "/images"));
-app.use(express.static(__dirname + '/public'));
-
 console.log(validator.isEmail("test@gmail.com"));
-console.log(validator.isCreditCard('123456789'));
 
 var log_text = "Test - Test - Test. "
 
-fs.writeFileSync('mylog.txt', log_text , (err) => {
+fs.writeFileSync('assignment_2.txt', log_text , (err) => {
 
     if (err) throw err;
     console.log('File Created');
-});
-
-app.set('port', process.env.PORT || 3001);
-
-app.get('/', function(req, res) {
-	res.render('home');
 });
 
     app.listen(app.get('port'), function(){
